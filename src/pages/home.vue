259b42bb584a479d3e0b9c3c7e2ecf248dafa0c1
Roger Castro <icarus.gk@gmail.com>
2025-05-18 19:43:55 -0600

    Initial commit: Vite + Vue 3 + Tailwind CSS template

---
+++ src/pages/home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen gap-2">
+    <h1 class="text-3xl font-bold">Welcome!</h1>
+    <button class="btn" @click="count++">Count is: {{ count }}</button>
+  </div>
+</template>
